c7a459f53ca701157e748156faa7ed9e52879024
Aji2324 <ajithece23@gmail.com>
2021-08-10 16:43:58 +0530

    A work

--- src/org/sample/Client.java
+++ src/org/sample/Client.java
@@ -1,20 +1,28 @@
 package org.sample;
 
-public class Client extends Company{
-public void clientId() {
-}
+public class Client extends Company {
+	public void clientId() {
+	}
 
-@Override
-public void custId() {
-	System.out.println("Custom Id:8687");
-	
-}
+	@Override
+	public void custId() {
+		System.out.println("Custom Id:8687");
 
-@Override
-public void custName() {
-	System.out.println("Custom Name is:SRM");
-	
-}
+	}
 
+	@Override
+	public void custName() {
+		System.out.println("Custom Name is:SRM");
 
+	}
+
+	public void custAdd() {
+		System.out.println("Customer address : Chennai");
+
+	}
+
+	public void custPhone() {
+		System.out.println("Customer Number : 1234567890");
+
+	}
 }
